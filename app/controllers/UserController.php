c9a306e1e1fee82d2e88a2816fe5d03be059c66b
Vidoje <vidoje.sevic.275.19@ict.edu.rs>
2023-09-18 15:23:20 +0200

    fixed security issue

---
+++ app/controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace app\controllers;
+
+class UserController
+{
+    // get and insert data from database
+}
+
+
--- app/models/NavigationModel.php
+++ app/models/NavigationModel.php
@@ -1,5 +1,8 @@
 <?php
+declare(strict_types=1);
+
 namespace App\Models;
+
 use PDOException;
 
 require_once('../../config/conn.php');
